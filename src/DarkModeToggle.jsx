8eb5ded0e3109020093ff44b8dc67144ca622469
donavon <github@donavon.com>
2019-01-14 23:48:44 -0500

    refactor to use object from useDarkMode

--- src/DarkModeToggle.jsx
+++ src/DarkModeToggle.jsx
@@ -4,22 +4,18 @@ import Toggle from './Toggle';
 import useDarkMode from 'use-dark-mode';
 
 const DarkModeToggle = () => {
-  const [isDarkMode, setDarkMode, clearDarkMode, toggleDarkMode] = useDarkMode(
-    false
-  );
+  const darkMode = useDarkMode(false);
 
   return (
-    <>
-      <div className="dark-mode-toggle">
-        <button type="button" onClick={clearDarkMode}>
-          ☀
-        </button>
-        <Toggle checked={isDarkMode} onChange={toggleDarkMode} />
-        <button type="button" onClick={setDarkMode}>
-          ☾
-        </button>
-      </div>
-    </>
+    <div className="dark-mode-toggle">
+      <button type="button" onClick={darkMode.disable}>
+        ☀
+      </button>
+      <Toggle checked={darkMode.value} onChange={darkMode.toggle} />
+      <button type="button" onClick={darkMode.enable}>
+        ☾
+      </button>
+    </div>
   );
 };
 
